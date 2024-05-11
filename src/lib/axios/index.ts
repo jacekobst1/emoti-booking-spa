86645303c336311769906e8b11b7c5c810788af7
Jacek Obst <jacek.obst@merito.pl>
2024-05-11 23:13:08 +0200

    feat: display toas on axios exception

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import config from "config";
+import fireToast from "lib/toast";
 
 const axiosInstance = axios.create({
     baseURL: config.apiUrl,
@@ -10,5 +11,22 @@ const axiosInstance = axios.create({
     },
     // withCredentials: true,
 });
+
+// Handle exceptions
+axiosInstance.interceptors.response.use(
+    (res) => res,
+    (error) => {
+        const {status} = error.response;
+
+        if ([400, 409, 422, 429, 500].includes(status)) {
+            let message = error.response.data.message;
+            if (status === 500) {
+                message = 'Something went wrong. Contact the administrator.';
+            }
+            void fireToast('error', message);
+            return Promise.reject(error);
+        }
+    }
+);
 
 export default axiosInstance;
