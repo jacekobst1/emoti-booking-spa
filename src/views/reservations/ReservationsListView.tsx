6e3be3412edf604203405b367417a7e295af8803
Jacek Obst <jacek.obst@merito.pl>
2024-05-11 20:30:53 +0200

    chore: install tailwindcss (with postcss and autoprefixer)

--- src/views/reservations/ReservationsListView.tsx
+++ src/views/reservations/ReservationsListView.tsx
@@ -2,6 +2,7 @@ function ReservationsListView() {
     return (
         <>
             <h1>Reservations list</h1>
+            <span className="text-3xl text-amber-400">Here is a list of all reservations</span>
         </>
     )
 }
